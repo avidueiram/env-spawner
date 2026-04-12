2ccce22413fa6b79df6e2804fbd954175d7226f9
Alfredo Vidueira <vidueira1992@gmail.com>
2026-04-11 20:58:20 -0400

    refactor(docker): update compose configuration and enhance start script for environment management
    - Replaced env-spawner service with environment-hub service using node:22-alpine image.
    - Added dynamic environment variable substitution in the environment-hub command.
    - Updated start.sh to accept an environment argument, load corresponding .env file, and open the environment hub in the default browser.
    - Included support for optional postcompose scripts in the start process.
    - Expanded .gitignore to include environment-specific files.

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,16 +2,39 @@
 
 source ./scripts/utils.sh
 project=$1
+environment=$2
 script_name=$(basename "$0")
 script_action=$(echo "$script_name" | cut -d'.' -f1)
 start_time=$(date +%s)
 
 print_start
 check_args "$project"
+check_args "$environment"
+
+source "environments/.env.$environment"
 
 echo "[$script_name]▶ Starting environment..."
-if ! docker compose -p "$project" -f "compose.yaml" -f "environments/$project.yaml" up -d; then
+if ! docker compose \
+    -p "$project" \
+    --env-file "environments/.env.$environment" \
+    -f "compose.yaml" \
+    -f "environments/$project.yaml" \
+    up -d; then
     print_error "Failed to start environment"
+fi
+
+echo "[$script_name]▶ Opening environment hub in browser..."
+if ls /Applications | grep -qi "Brave Browser"; then
+    open -na "Brave Browser" --args --new-window "http://localhost:${ENVIRONMENT_HUB_PORT}/"
+else
+    open -na "Google Chrome" --args --new-window "http://localhost:${ENVIRONMENT_HUB_PORT}/"
+fi
+
+if [ -f "environments/$project.postcompose.sh" ]; then
+    echo "[$script_name]▶ Running postcompose script..."
+    source "environments/$project.postcompose.sh"
+else
+    echo "[$script_name]▶ No postcompose script found"
 fi
 
 print_finish
